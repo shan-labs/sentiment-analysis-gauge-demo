0f0b56321b47ad346e405710dfcb3015c898f814
Khinshan Khan <khanshan501@gmail.com>
2020-07-01 16:15:58 -0400

    simple data pass

--- client/src/components/display/SentimentOutput.jsx
+++ client/src/components/display/SentimentOutput.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 export default class SentimentOutput extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return <div>Hi</div>;
   }
