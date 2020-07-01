5a77171971eb7cd4e0a7c22a506826dd7d545cec
Khinshan Khan <khanshan501@gmail.com>
2020-07-01 16:28:00 -0400

    refactor gauge to be more reusable

---
+++ client/src/components/display/Gauge.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import ReactSpeedometer from "react-d3-speedometer";
+
+export default class Gauge extends Component {
+  render() {
+    return (
+      <ReactSpeedometer
+        width={500}
+        needleHeightRatio={0.7}
+        value={this.props.value}
+        currentValueText="Sentiment Level"
+        customSegmentLabels={[
+          {
+            text: "Very Low",
+            position: "INSIDE",
+            color: "#555"
+          },
+          {
+            text: "Low",
+            position: "INSIDE",
+            color: "#555"
+          },
+          {
+            text: "Ok",
+            position: "INSIDE",
+            color: "#555",
+            fontSize: "19px"
+          },
+          {
+            text: "High",
+            position: "INSIDE",
+            color: "#555"
+          },
+          {
+            text: "Very High",
+            position: "INSIDE",
+            color: "#555"
+          }
+        ]}
+        ringWidth={47}
+        needleTransitionDuration={3333}
+        needleTransition="easeElastic"
+        needleColor={"#90f2ff"}
+        textColor={"#d8dee9"}
+      />
+    );
+  }
+}
--- client/src/components/display/SentimentOutput.jsx
+++ client/src/components/display/SentimentOutput.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import ReactSpeedometer from "react-d3-speedometer";
+import Gauge from "./Gauge";
 
 const initialEmotions = {
   sadness: 0,
@@ -19,45 +19,7 @@ export default class SentimentOutput extends Component {
   render() {
     return (
       <>
-        <ReactSpeedometer
-          width={500}
-          needleHeightRatio={0.7}
-          value={777}
-          currentValueText="Sentiment Level"
-          customSegmentLabels={[
-            {
-              text: "Very Low",
-              position: "INSIDE",
-              color: "#555"
-            },
-            {
-              text: "Low",
-              position: "INSIDE",
-              color: "#555"
-            },
-            {
-              text: "Ok",
-              position: "INSIDE",
-              color: "#555",
-              fontSize: "19px"
-            },
-            {
-              text: "High",
-              position: "INSIDE",
-              color: "#555"
-            },
-            {
-              text: "Very High",
-              position: "INSIDE",
-              color: "#555"
-            }
-          ]}
-          ringWidth={47}
-          needleTransitionDuration={3333}
-          needleTransition="easeElastic"
-          needleColor={"#90f2ff"}
-          textColor={"#d8dee9"}
-        />
+        <Gauge value={100} />
       </>
     );
   }
